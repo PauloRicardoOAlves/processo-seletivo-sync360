1b415f02b390f05fde7a1b7a9a573f32351f5d31
Paulo Ricardo Alves <pauloricardooalves@gmail.com>
2024-05-09 17:37:29 -0300

    faz adequações no código e insere o readme

--- BackEnd/src/schemas/formSchema.js
+++ BackEnd/src/schemas/formSchema.js
@@ -7,28 +7,28 @@ const formSchema = joi.object({
     }),
     age: joi.number().positive().required().messages({
         "any.required": "O campo IDADE é obrigatório!",
-        "number.base": "O campo IDADE não deve ser em branco",
+        "number.base": "O campo IDADE é obrigatório!",
         "number.positive": "O campo IDADE deve ser maior que zero!"
     }),
     adress: joi.string().required().messages({
         "any.required": "Preencha o campo ENDEREÇO nome para prosseguir",
-        "string.empty": "O campo ENDEREÇO não pode ficar em branco"
+        "string.empty": "O campo ENDEREÇO é obrigatório!"
     }),
     adress2: joi.string().required().messages({
         "any.required": "Preencha o campo BAIRRO nome para prosseguir",
-        "string.empty": "O campo BAIRRO não pode ficar em branco"
+        "string.empty": "O campo BAIRRO é obrigatório!"
     }),
     city: joi.string().required().messages({
         "any.required": "Preencha o campo CIDADE nome para prosseguir",
-        "string.empty": "O campo CIDADE não pode ficar em branco"
+        "string.empty": "O campo CIDADE é obrigatório!"
     }),
     state: joi.string().required().messages({
         "any.required": "Preencha o campo ESTADO nome para prosseguir",
-        "string.empty": "O campo ESTADO não pode ficar em branco"
+        "string.empty": "O campo ESTADO é obrigatório!"
     }),
     bio: joi.string().required().messages({
         "any.required": "Preencha o campo BIO nome para prosseguir",
-        "string.empty": "O campo nome BIO pode ficar em branco"
+        "string.empty": "O campo BIO é obrigatório!"
     }),
 })
 
